8386908a8cf8196f96ca5a51fbc5b7fa298f6500
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-11 10:36:51 +0300

    Create settings panel, image presentation

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -3,8 +3,10 @@ import { FC, useState } from 'react';
 import styles from './assets/ImageUploader.module.css';
 
 const ImageUploader: FC = () => {
+  const MAX_IMAGE_SIZE = 5 * 1024 * 1024
   const [image, setImage] = useState<string | null>(null);
   const [dragOver, setDragOver] = useState(false);
+  const [isUploaded, setUploadedStatus] = useState(false)
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -14,14 +16,20 @@ const ImageUploader: FC = () => {
   };
 
   const handleFileUpload = (file: File) => {
-    if (file.type.startsWith('image/')) {
+    if (file.size > MAX_IMAGE_SIZE) {
+      alert("Размер файла не должен превышать 5 МБ.");
+      return;
+    }
+
+    if (file.type.startsWith("image/")) {
       const reader = new FileReader();
       reader.onload = () => {
         setImage(reader.result as string);
+        setUploadedStatus(true)
       };
       reader.readAsDataURL(file);
     } else {
-      alert('Пожалуйста, загрузите изображение.');
+      alert("Пожалуйста, загрузите изображение.");
     }
   };
 
@@ -43,17 +51,38 @@ const ImageUploader: FC = () => {
     setDragOver(false);
   };
 
+  const handleDeleteImage = () => {
+    setImage(null);
+    setUploadedStatus(false);
+  };
+
+  const handleReplaceImage = () => {
+    document.getElementById('fileInput')?.click();
+  };
+
+  const handleOpenEditor = () => {
+    // pending
+  };
+
   return (
     <div className={styles.uploadContainer}>
       <div 
-        className={`${styles.uploadZone} ${dragOver ? styles.dragOver : ""}`}
-        onClick={() => document.getElementById("fileInput")?.click()}
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
+        className={`${styles.uploadZoneSize} ${dragOver ? styles.dragOver : ""} ${!isUploaded ? styles.uploadZoneAppearance : ""}`}
+        onClick={!isUploaded ? () => document.getElementById('fileInput')?.click() : undefined}
+        onDrop={!isUploaded ? handleDrop : undefined}
+        onDragOver={!isUploaded ? handleDragOver : undefined}
+        onDragLeave={!isUploaded ? handleDragLeave : undefined}
         >
           {image ? (
-            <img src={image} alt="Uploaded" className={styles.uploadedImage} />
+            <>
+              <div className={styles.settingsPanel}>
+                <span>Image</span>
+                <button onClick={handleReplaceImage} className={styles.toolButton}>Replace</button>
+                <button onClick={handleOpenEditor} className={styles.toolButton}>Edit</button>
+                <button onClick={handleDeleteImage} className={styles.toolButton}>Delete</button>
+              </div>
+              <img src={image} alt="Uploaded" className={styles.uploadedImage} />
+            </>
           ) : (
             <p>Перетащите сюда изображение или нажмите, чтобы выбрать файл</p>
           )}
