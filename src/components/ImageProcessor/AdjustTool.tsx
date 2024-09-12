71905c3609ef6a8724b9268fbee45f4afe33214f
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-12 22:25:10 +0300

    Add adjust component

--- src/components/ImageProcessor/AdjustTool.tsx
+++ src/components/ImageProcessor/AdjustTool.tsx
@@ -9,11 +9,13 @@ interface AdjustToolProps {
   contrast: number;
   saturation: number;
   exposure: number;
+  onImageAdjusted: (newImage: HTMLImageElement) => void;
 }
 
-const AdjustTool: React.FC<AdjustToolProps> = ({ imageSrc, brightness, contrast, saturation, exposure }) => {
+const AdjustTool: React.FC<AdjustToolProps> = ({ imageSrc, brightness, contrast, saturation, exposure, onImageAdjusted }) => {
   const [image, setImage] = useState<HTMLImageElement | null>(null);
   const imageRef = useRef<Konva.Image>(null);
+  const stageRef = useRef<Konva.Stage>(null);
 
   // Загружаем изображение при первом рендере
   useEffect(() => {
@@ -35,9 +37,22 @@ const AdjustTool: React.FC<AdjustToolProps> = ({ imageSrc, brightness, contrast,
     }
   }, [brightness, contrast, saturation, exposure]);
 
+  useEffect(() => {
+    if (image && stageRef.current) {
+      const stage = stageRef.current;
+      const dataURL = stage.toDataURL({ pixelRatio: 3 }); // Увеличение качества изображения
+      const newImage = new Image();
+      newImage.src = dataURL;
+
+      newImage.onload = () => {
+        onImageAdjusted(newImage);
+      };
+    }
+  }, [image, brightness, contrast, saturation, exposure, onImageAdjusted]);
+
   return (
     <div className={styles.adjustTool}>
-      <Stage width={500} height={500}>
+      <Stage width={500} height={500} ref={stageRef}>
         <Layer>
           {image && (
             <KonvaImage
