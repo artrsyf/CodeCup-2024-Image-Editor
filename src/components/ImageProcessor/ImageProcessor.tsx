d245b064e88ce5ef8efdf2628063fffdd3b55369
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-13 10:47:03 +0300

    Refacor ImageProcessor component

--- src/components/ImageProcessor/ImageProcessor.tsx
+++ src/components/ImageProcessor/ImageProcessor.tsx
@@ -10,6 +10,8 @@ import TextSettingsMenu from './TextSettingsMenu';
 import ShapeSettingsMenu from "./ShapeSettingsMenu"
 import FilterTool from "./FilterTool"
 
+import {renderTextEditArea, produceResizedTempImage, produceRotatedTempImage} from "./utils/utils"
+
 interface ImageProcessorProps {
   imageSrc: string;
   onCancel: () => void;
@@ -50,12 +52,6 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
   const transformerRef = useRef<Konva.Transformer>(null);
   const stageRef = useRef<any>(null);
 
-  // ????
-
-
-  //
-  
-
   const [editingText, setEditingText] = useState<any>(null);
 
   // Начальная установка текущей пикчи
@@ -70,51 +66,6 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
       setResizeWidth(img.width);
     };
   }, [imageSrc]);
-
-  const handleToolClick = (tool: string) => {
-    setActiveTool(tool);
-  };
-
-  const handleScaleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const scale = parseFloat(event.target.value);
-    setCropScale(scale);
-  };
-
-  const handleResizeWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setResizeWidth(Number(event.target.value));
-  };
-
-  const handleResizeHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setResizeHeight(Number(event.target.value));
-  };
-
-  const handleAspectRatioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPreserveAspectRatio(event.target.checked);
-  };
-
-  const handleRotateRight = () => {
-    setRotateAngle((prevAngle) => (prevAngle + 90) % 360);
-  };
-
-  const handleRotateLeft = () => {
-    setRotateAngle((prevAngle) => (prevAngle - 90 + 360) % 360);
-  };
-
-  const handleFlipHorizontal = () => {
-    setFlipHorizontal((prev) => !prev);
-  };
-
-  const handleFlipVertical = () => {
-    setFlipVertical((prev) => !prev);
-  };
-
-  const applyFilter = (filter: string) => {
-    setActiveFilter(filter);
-  };
-
-  const toggleElementsModal = () => {
-    setShowElementsModal((prev) => !prev);
-  };
 
   const addElement = (type: string) => {
     let newElement;
@@ -236,178 +187,9 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
 
   useEffect(() => {
     if (editingText) {
-      renderTextEditArea();
+      renderTextEditArea(editingText, stageRef, setEditingText);
     }
   }, [editingText]);
-
-  const handleTextDblClick = (element: any) => {
-    setEditingText(element);
-  };
-  
-  const renderTextEditArea = () => {
-    if (!editingText) return null;
-
-    var stage = stageRef.current;
-    var textNode = stageRef.current?.findOne(`#${editingText.id}`);
-    if (!textNode) return null;
-
-    var tr = new Konva.Transformer({
-      node: textNode,
-      enabledAnchors: ['middle-left', 'middle-right'],
-      // set minimum width of text
-      boundBoxFunc: function (oldBox, newBox) {
-        newBox.width = Math.max(30, newBox.width);
-        return newBox;
-      },
-    });
-
-    textNode.on('transform', function () {
-      // reset scale, so only with is changing by transformer
-      textNode.setAttrs({
-        width: textNode.width() * textNode.scaleX(),
-        scaleX: 1,
-      });
-    });
-
-    textNode.hide();
-    tr.hide();
-
-    var textPosition = textNode.absolutePosition();
-
-    // so position of textarea will be the sum of positions above:
-    var areaPosition = {
-      x: stage.container().offsetLeft + textPosition.x,
-      y: stage.container().offsetTop + textPosition.y,
-    };
-
-    var textarea = document.createElement('textarea');
-    document.body.appendChild(textarea);
-
-    textarea.value = textNode.text();
-    textarea.style.position = 'absolute';
-    textarea.style.top = areaPosition.y + 'px';
-    textarea.style.left = areaPosition.x + 'px';
-    textarea.style.width = textNode.width() - textNode.padding() * 2 + 'px';
-    textarea.style.height =
-      textNode.height() - textNode.padding() * 2 + 5 + 'px';
-    textarea.style.fontSize = textNode.fontSize() + 'px';
-    textarea.style.border = 'none';
-    textarea.style.padding = '0px';
-    textarea.style.margin = '0px';
-    textarea.style.overflow = 'hidden';
-    textarea.style.background = 'none';
-    textarea.style.outline = 'none';
-    textarea.style.resize = 'none';
-    textarea.style.lineHeight = textNode.lineHeight();
-    textarea.style.fontFamily = textNode.fontFamily();
-    textarea.style.transformOrigin = 'left top';
-    textarea.style.textAlign = textNode.align();
-    textarea.style.color = textNode.fill();
-    const rotation = textNode.rotation();
-    var transform = '';
-    if (rotation) {
-      transform += 'rotateZ(' + rotation + 'deg)';
-    }
-
-    var px = 0;
-    // also we need to slightly move textarea on firefox
-    // because it jumps a bit
-    var isFirefox =
-      navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-    if (isFirefox) {
-      px += 2 + Math.round(textNode.fontSize() / 20);
-    }
-    transform += 'translateY(-' + px + 'px)';
-
-    textarea.style.transform = transform;
-
-    const updateSizes = () => {
-      textarea.style.height = 'auto';
-      textarea.style.height = textarea.scrollHeight + 'px';
-
-      textNode.width(textarea.offsetWidth);
-      textNode.height(textarea.offsetHeight);
-      textNode.getLayer()?.batchDraw(); // Ensure the layer is updated
-    };
-
-    textarea.addEventListener('input', updateSizes);
-
-    // reset height
-    textarea.style.height = 'auto';
-    // after browsers resized it we can set actual value
-    textarea.style.height = textarea.scrollHeight + 3 + 'px';
-
-    textarea.focus();
-
-    function removeTextarea() {
-      textarea.parentNode?.removeChild(textarea);
-      window.removeEventListener('click', handleOutsideClick);
-      textNode.show();
-      tr.show();
-      tr.forceUpdate();
-    }
-
-    function setTextareaWidth(newWidth: any) {
-      if (!newWidth) {
-        // set width for placeholder
-        newWidth = textNode.placeholder.length * textNode.fontSize();
-      }
-      // some extra fixes on different browsers
-      var isSafari = /^((?!chrome|android).)*safari/i.test(
-        navigator.userAgent
-      );
-      var isFirefox =
-        navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-      if (isSafari || isFirefox) {
-        newWidth = Math.ceil(newWidth);
-      }
-      
-      // @ts-ignore
-      var isEdge = !!document.documentMode || /Edge/.test(navigator.userAgent);
-      if (isEdge) {
-        newWidth += 1;
-      }
-      textarea.style.width = newWidth + 'px';
-    }
-
-    textarea.addEventListener('keydown', function (e: KeyboardEvent) {
-      // hide on enter
-      // but don't hide on shift + enter
-      if (e.keyCode === 13 && !e.shiftKey) {
-        textNode.text(textarea.value);
-        removeTextarea();
-        setEditingText(null);
-      }
-      // on esc do not set value back to node
-      if (e.keyCode === 27) {
-        removeTextarea();
-        setEditingText(null);
-      }
-    });
-
-    textarea.addEventListener('keydown', function (e) {
-      const scale = textNode.getAbsoluteScale().x;
-      setTextareaWidth(textNode.width() * scale);
-      textarea.style.height = 'auto';
-      textarea.style.height =
-        textarea.scrollHeight + textNode.fontSize() + 'px';
-    });
-
-    function handleOutsideClick(e: MouseEvent) {
-      if (e.target !== textarea) {
-        textNode.text(textarea.value);
-        removeTextarea();
-        setEditingText(null);
-      }
-    }
-    setTimeout(() => {
-      window.addEventListener('click', handleOutsideClick);
-    });
-  };
-
-  const handleCloseMenu = () => {
-    setSelectedElement(null);
-  };
 
   const renderRightPanel = () => {
     switch (activeTool) {
@@ -417,7 +199,7 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
             <label htmlFor="scaleSelect" className={styles.scaleLabel}>
               Crop ratio
             </label>
-            <select id="scaleSelect" onChange={handleScaleChange} className={styles.scaleSelect}>
+            <select id="scaleSelect" onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {setCropScale(parseFloat(event.target.value));}} className={styles.scaleSelect}>
               <option value="1">1:1</option>
               <option value="1.778">16:9</option>
               <option value="1.333">4:3</option>
@@ -430,14 +212,16 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
             <input
               type="number"
               value={resizeWidth}
-              onChange={handleResizeWidthChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => 
+                setResizeWidth(Number(event.target.value))}
               placeholder="Width"
               className={styles.inputField}
             />
             <input
               type="number"
               value={resizeHeight}
-              onChange={handleResizeHeightChange}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setResizeHeight(Number(event.target.value))}
               placeholder="Height"
               className={styles.inputField}
             />
@@ -445,7 +229,8 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
               <input
                 type="checkbox"
                 checked={preserveAspectRatio}
-                onChange={handleAspectRatioChange}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setPreserveAspectRatio(event.target.checked)}
               />
               Constrain proportions
             </label>
@@ -456,14 +241,22 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
           <div className={styles.rotateFlipControls}>
             <div>Rotate</div>
             <div className={styles.rotateControls}>
-              <button className={styles.toolButton} onClick={handleRotateLeft}>Rotate Left</button>
-              <button className={styles.toolButton} onClick={handleRotateRight}>Rotate Right</button>
+              <button className={styles.toolButton} onClick={() =>
+                setRotateAngle((prevAngle) => (prevAngle - 90 + 360) % 360)
+              }>Rotate Left</button>
+              <button className={styles.toolButton} onClick={() => 
+                setRotateAngle((prevAngle) => (prevAngle + 90) % 360)
+              }>Rotate Right</button>
             </div>
 
             <div>Flip</div>
             <div className={styles.flipControls}>
-              <button className={styles.toolButton} onClick={handleFlipHorizontal}>Flip Horizontal</button>
-              <button className={styles.toolButton} onClick={handleFlipVertical}>Flip Vertical</button>
+              <button className={styles.toolButton} onClick={() =>
+                setFlipHorizontal((prev) => !prev)
+              }>Flip Horizontal</button>
+              <button className={styles.toolButton} onClick={() => 
+                setFlipVertical((prev) => !prev)
+              }>Flip Vertical</button>
             </div> 
           </div>
         );
@@ -483,10 +276,10 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
       case 'filters':
         return (
           <div className={styles.filterGrid}>
-          <button className={styles.toolButton} onClick={() => applyFilter('none')}>None</button>
-          <button className={styles.toolButton} onClick={() => applyFilter('grayscale')}>Black&White</button>
-          <button className={styles.toolButton} onClick={() => applyFilter('sepia')}>Sepia</button>
-          <button className={styles.toolButton} onClick={() => applyFilter('vintage')}>Vintage</button>
+          <button className={styles.toolButton} onClick={() => setActiveFilter("none")}>None</button>
+          <button className={styles.toolButton} onClick={() => setActiveFilter("grayscale")}>Black&White</button>
+          <button className={styles.toolButton} onClick={() => setActiveFilter("sepia")}>Sepia</button>
+          <button className={styles.toolButton} onClick={() => setActiveFilter("vintage")}>Vintage</button>
         </div>
         );
       case 'elements':
@@ -495,14 +288,14 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
             return (
               <TextSettingsMenu
                 textNode={stageRef.current?.findOne(`#${selectedElement.id}`)}
-                onClose={handleCloseMenu}
+                onClose={() => setSelectedElement(null)}
               />
             );
           } else {
               return (
                 <ShapeSettingsMenu
                     shapeNode={stageRef.current?.findOne(`#${selectedElement.id}`)}
-                    onClose={handleCloseMenu}
+                    onClose={() => setSelectedElement(null)}
                 />
               );
           }
@@ -527,65 +320,12 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
   };
 
   useEffect(() => {
-    produceRotatedImage()
+    produceRotatedTempImage(currentImage, canvasRef, rotateAngle, flipHorizontal, flipVertical, setTempImage)
   }, [flipHorizontal, flipVertical, rotateAngle])
 
   useEffect(() => {
-    produceNewTempImage()
+    produceResizedTempImage(currentImage, canvasRef, resizeHeight, resizeHeight, setTempImage)
   }, [resizeHeight, resizeWidth, preserveAspectRatio])
-
-  useEffect(() => {
-    produceAdjustedTempImage()
-  }, [brightness, contrast, saturation, exposure])
-
-  const produceNewTempImage = () => {
-    if (currentImage) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        canvas.width = resizeWidth;
-        canvas.height = resizeHeight;
-        ctx.drawImage(currentImage, 0, 0, resizeWidth, resizeHeight);
-
-        const newImageSrc = canvas.toDataURL('image/jpeg');
-        const newImage = new Image();
-        newImage.src = newImageSrc;
-
-        setTempImage(newImage);
-      }
-    }
-  };
-
-  const produceRotatedImage = () => {
-    if (currentImage) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        const width = currentImage.width;
-        const height = currentImage.height;
-
-        canvas.width = width;
-        canvas.height = height;
-
-        ctx.save();
-        ctx.translate(width / 2, height / 2);
-        ctx.rotate((rotateAngle * Math.PI) / 180);
-        ctx.scale(flipHorizontal ? 1 : -1, flipVertical ? 1 : -1);
-        ctx.drawImage(currentImage, -width / 2, -height / 2);
-        ctx.restore();
-
-        const newImageSrc = canvas.toDataURL('image/jpeg');
-        const newImage = new Image();
-        newImage.src = newImageSrc;
-
-        setTempImage(newImage);
-      }
-    }
-  };
-
-  const produceAdjustedTempImage = () => {
-
-  }
   
   return (
     <div className={styles.imageProcessorContainer}>
@@ -596,14 +336,14 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
       <div className={styles.contentWrapper}>
         <canvas ref={canvasRef} style={{ display: 'none' }} />
         <div className={styles.leftMenu}>
-          <button className={styles.toolButton} onClick={() => handleToolClick('crop')}>Crop</button>
-          <button className={styles.toolButton} onClick={() => handleToolClick('resize')}>Resize</button>
-          <button className={styles.toolButton} onClick={() => handleToolClick('rotate')}>Rotate and flip</button>
-          <button className={styles.toolButton} onClick={() => handleToolClick('adjust')}>Adjust</button>
-          <button className={styles.toolButton} onClick={() => handleToolClick('filters')}>Filters</button>
+          <button className={styles.toolButton} onClick={() => setActiveTool('crop')}>Crop</button>
+          <button className={styles.toolButton} onClick={() => setActiveTool('resize')}>Resize</button>
+          <button className={styles.toolButton} onClick={() => setActiveTool('rotate')}>Rotate and flip</button>
+          <button className={styles.toolButton} onClick={() => setActiveTool('adjust')}>Adjust</button>
+          <button className={styles.toolButton} onClick={() => setActiveTool('filters')}>Filters</button>
           <button className={styles.toolButton} onClick={() => {
-            handleToolClick("elements")
-            toggleElementsModal()
+            setActiveTool("elements")
+            setShowElementsModal((prev) => !prev);
           }}>
             Elements
           </button>
@@ -674,15 +414,15 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
             />
           )}
           {activeTool === 'elements' && (
-            <Stage width={800} height={600} ref={stageRef} onMouseDown={(e) => e.target === e.target.getStage() && setSelectedElement(null)}>
+            <Stage width={currentImage?.width} height={currentImage?.height} ref={stageRef} onMouseDown={(e) => e.target === e.target.getStage() && setSelectedElement(null)} perfectDrawEnabled={true} pixelRatio={window.devicePixelRatio}>
               <Layer>
                 {imageSrc && (
                   <KonvaImage
                     image={currentImage}
                     x={0}
                     y={0}
-                    width={800}
-                    height={600}
+                    width={currentImage?.width}
+                    height={currentImage?.height}
                     listening={false} // Disable interactions with the image
                   />
                 )}
@@ -701,8 +441,8 @@ const ImageProcessor: FC<ImageProcessorProps> = ({ imageSrc, onCancel }) => {
                             }}
                             onTransformEnd={handleTransform}
                             onDragEnd={handleDragEnd}
-                            onDblClick={() => handleTextDblClick(element)}
-                            onDblTap={() => handleTextDblClick(element)}
+                            onDblClick={() => setEditingText(element)}
+                            onDblTap={() => setEditingText(element)}
                             id={element.id}
                           />
                         </React.Fragment>
