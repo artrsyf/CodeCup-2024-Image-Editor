d5e8d880b6a5eb2a8fc70f2a3d2217310fe76836
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-14 19:35:08 +0300

    Delete pending components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import Layout from "./pages/Layout/Layout"
@@ -10,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<MainPage />} />
-          {/* <Route path="/users" element={<Users />} />
-          <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
     </Router>
