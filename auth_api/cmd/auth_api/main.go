503a6decefa4bb464b37b8002edfc92dd0c6b4e2
artrsyf <rom.fadeev2017@yandex.ru>
2024-09-26 13:57:02 +0300

    Implement minio uploading

--- auth_api/cmd/auth_api/main.go
+++ auth_api/cmd/auth_api/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
 
+	ImageRepository "auth_api/pkg/image/repository/minio"
 	sessionRepository "auth_api/pkg/session/repository/redis"
 	userRepository "auth_api/pkg/user/repository/mysql"
 
+	imageDelivery "auth_api/pkg/image/delivery"
 	userDelivery "auth_api/pkg/user/delivery"
 
 	"auth_api/pkg/middleware"
@@ -20,8 +24,46 @@ import (
 	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/rs/cors"
 )
+
+func initMinIO() *minio.Client {
+	var minioClient *minio.Client
+	var bucketName = "images"
+
+	endpoint := "localhost:9001"
+	accessKeyID := "minioadmin"
+	secretAccessKey := "minioadmin"
+	useSSL := false
+
+	// Инициализация MinIO клиента
+	var err error
+	minioClient, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Создание бакета, если его нет
+	err = minioClient.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{Region: "us-east-1"})
+	if err != nil {
+		// Проверяем, существует ли бакет
+		exists, errBucketExists := minioClient.BucketExists(context.Background(), bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("Bucket %s уже существует\n", bucketName)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Успешно создан бакет %s\n", bucketName)
+	}
+
+	return minioClient
+}
 
 func main() {
 	tools.Init()
@@ -29,6 +71,8 @@ func main() {
 	if err != nil {
 		slog.Error("Cant load .env file")
 	}
+
+	minio := initMinIO()
 
 	router := mux.NewRouter()
 
@@ -64,10 +108,16 @@ func main() {
 
 	userRepo := userRepository.NewUserMySqlRepo(mysqlConnect)
 	sessionRepo := sessionRepository.NewSessionRedisManager(redisConn)
+	imageRepo := ImageRepository.NewImageMinioRepository(minio)
 
 	authHandler := userDelivery.UserHandler{
 		UserRepo:    userRepo,
 		SessionRepo: sessionRepo,
+	}
+
+	imageHandler := imageDelivery.ImageHandler{
+		UserRepo:  userRepo,
+		ImageRepo: imageRepo,
 	}
 
 	router.Handle("/api/login", middleware.ValidateContentType(
@@ -83,6 +133,9 @@ func main() {
 		middleware.ValidateJWTToken(
 			sessionRepo,
 			http.HandlerFunc(authHandler.GetAccess))).Methods("GET")
+
+	router.Handle("/api/upload",
+		http.HandlerFunc(imageHandler.UploadImage)).Methods("POST")
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:5173"}, // Указываем фронтенд адрес
--- auth_api/go.mod
+++ auth_api/go.mod
@@ -11,10 +11,21 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/minio-go/v7 v7.0.77 // indirect
 	github.com/rs/cors v1.11.1 // indirect
+	github.com/rs/xid v1.6.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
